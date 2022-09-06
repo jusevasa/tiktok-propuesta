2154c3338455aadcbe9a8726cbfc11b45beda41a
Juan Sebastian Valdes <jvaldes@robinfood.com>
2022-09-06 12:54:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import Swal from 'sweetalert2'
+
 import './App.css';
 
 function App() {
+
+  const handleMoveButton = (e) => {
+    let x = Math.round(Math.random() * 94)
+    let y = Math.round(Math.random() * 94)
+
+    e.target.style = `left:${x}% ; top:${y}%; position:absolute`
+  }
+
+  const handleClickBtn = () => {
+    Swal.fire({
+      title: 'Lo sabia 👀',
+      text: 'Ya estas comprometida bb 🥴',
+      icon: 'success',
+      confirmButtonText: 'Cool'
+    }).then(() => {
+      window.location.href = 'https://www.youtube.com/watch?v=rzNiP2fbUio&ab_channel=CloudyNetwork'
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>¿Quieres ser mi novia?</h1>
+      <div className="container">
+        <button onClick={handleClickBtn}>Si</button>
+        <button onMouseMove={(e) => handleMoveButton(e)}>No</button>
+      </div>
     </div>
   );
 }
